3b9a7232a3a54b061b902d204c4f454918a35a40
sunnysequeira <ses9555@nyu.edu>
2022-12-15 22:36:57 -0500

    finalproject

---
+++ finalproject/aboutpage.js
@@ -0,0 +1,4 @@
+var cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", function(x){
+    cursor.style.cssText="left: " + x.clientX + "px; top: " + x.clientY + "px;";
+});
